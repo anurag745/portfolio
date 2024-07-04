93a79d3cd3675aa2841df6b383555c0e4c721d71
Anurag Sharma <tlannister745@gmail.com>
2024-07-04 19:15:49 +0530

    ab

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,22 @@
-import { useState } from 'react'
-import './index.css'
-import Home from './routes/Home.jsx'
-import About from './routes/About.jsx'
-import Contact from './routes/Contact.jsx'
-import Projects from './routes/Projects.jsx'
-import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
-
+import { useState } from 'react';
+import './index.css';
+import Home from './routes/Home.jsx';
+import About from './routes/About.jsx';
+import Contact from './routes/Contact.jsx';
+import Projects from './routes/Projects.jsx';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
-
   return (
-    <>
     <Router>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />}/>
-      <Route path="/projects" element={<Projects />} />
-      <Route path= "/contact" element={<Contact />} />
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </Router>
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
